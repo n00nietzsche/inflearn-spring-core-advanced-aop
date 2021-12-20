1d5590e35533654a20d82255fc70a44f44b4d46f
n00nietzsche <n00nietzsche@gmail.com>
2021-12-20 22:37:34 +0900

    스프링 핵심 원리 - 고급편 - 스프링 AOP - 포인트컷 - execution - 1

--- src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -2,11 +2,14 @@ package hello.aop.pointcut;
 
 import hello.aop.member.MemberServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
 import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 public class ExecutionTest {
@@ -25,4 +28,80 @@ public class ExecutionTest {
         // 여태까지 사용했던 execution 은 위에서 우리가 `.getMethod()` 메서드로 클래스 내부 메서드를 찾았던 것과 동일하게 메서드를 찾아낸다.
         log.info("helloMethod={}", helloMethod);
     }
+
+    @Test
+    // 아주 정확히 일치하는 예
+    void exactMatch() {
+        // public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
+        pointcut.setExpression("execution(public String hello.aop.member.MemberServiceImpl.hello(String))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void allMatch() {
+        // 반환 타입(return-type-pattern) = *, 이름 패턴(name-pattern) = *
+        pointcut.setExpression("execution(* *(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void nameMatch() {
+        // 반환 타입(return-type-pattern) = *, 이름 패턴(name-pattern) = hello
+        pointcut.setExpression("execution(* hello(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void namePatternMatch() {
+        // 반환 타입(return-type-pattern) = *, 이름 패턴(name-pattern) = hel*
+        pointcut.setExpression("execution(* hel*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void namePatternMatch2() {
+        // 반환 타입(return-type-pattern) = *, 이름 패턴(name-pattern) = *el*
+        pointcut.setExpression("execution(* *el*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void nameMatchFalse() {
+        // 반환 타입(return-type-pattern) = *, 이름 패턴(name-pattern) = eel*
+        pointcut.setExpression("execution(* eel*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    void packageExactMatch() {
+        // 패키지를 정확하게 매칭
+        pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.hello(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void packageExactFalse() {
+        // 패키지를 정확하게 매칭
+        // .이 하나일 때는, 바로 하위의 패키지만 가리키기 때문에 false 가 나온다.
+        pointcut.setExpression("execution(* hello.aop.*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    void packageMatchSubPackage1() {
+        // 패키지를 정확하게 매칭
+        // .. 두개 쓰면 모든 하위 패키지까지 가리킨다.
+        pointcut.setExpression("execution(* hello.aop..*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void packageMatchSubPackage2() {
+        // 패키지를 정확하게 매칭
+        // .. 두개 쓰면 모든 하위 패키지까지 가리킨다.
+        pointcut.setExpression("execution(* hello.aop.member..*.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+
 }
